e8b206b1bdbc0eac8d578710e3731c51cb3f8ba7
JancoNel <neljanco92@gmail.com>
2025-04-30 14:18:20 +0200

    Help, wtf is cmake doing

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+// Core
+#include "Core.hpp"
 #include "Config.hpp"
 #include "Utils.hpp"
+// Components
+#include "BlueShell.hpp"
+#include "FirewallBouncer.hpp"
+#include "ProcessSniper.hpp"
+#include "ServiceDisabler.hpp"
+#include "ServiceSpoofer.hpp"
+// Libraries
+
+using namespace std;
 
